8e5d37b81c2f779d73f58a10b5b47860b20d6035
king of greatness <124377851+kingofgreatnezzz@users.noreply.github.com>
2024-04-24 09:42:19 -0700

    Update settings.py
    cors aorigin port

--- back/settings.py
+++ back/settings.py
@@ -149,6 +149,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ### Cors header permissions
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
