6e4cbf6ed19d86efa2091f87d504c8f0f226e49f
king of greatness <124377851+kingofgreatnezzz@users.noreply.github.com>
2024-04-25 02:49:36 -0700

    Update settings.py

--- back/settings.py
+++ back/settings.py
@@ -147,12 +147,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-### Cors header permissions
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
